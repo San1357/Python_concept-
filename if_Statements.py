5f4e8ece3a00a947637daca6e5a144f1be743035
San Ayush <41052723+San1357@users.noreply.github.com>
2021-02-15 18:00:26 +0530

    Create if_Statements.py

---
+++ if_Statements.py
@@ -0,0 +1,12 @@
+# if Statement is a Conditional statement.
+
+# Let's do understand with program directly.
+
+x = int(input("Enter a integer number:"))
+if x < 0:
+    print("the number is negative integer")
+elif x == 0:
+    print("the number is Zero(0)")
+else:
+    print("the number is positive")
+    
