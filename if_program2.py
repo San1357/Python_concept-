016b6a2931a4c25d9d8c0910d8c0aacb326523cf
San Ayush <41052723+San1357@users.noreply.github.com>
2021-02-15 18:21:14 +0530

    Create if_program2.py

---
+++ if_program2.py
@@ -0,0 +1,11 @@
+Problem2:
+
+Take two int values from user and print greatest among them
+
+a = int(input("enter any integer number:"))
+b = int(input("enter any integer number:"))
+if a < b:
+    print("the greatest number among them is :",b)
+else:
+    print("the greatest number among them is:",a)
+    
