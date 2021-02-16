d8fe176de8f189f0567982f2f62548a5f2d595e7
San Ayush <41052723+San1357@users.noreply.github.com>
2021-02-16 12:17:11 +0530

    Update For_program7.py

--- For_Statements/For_program7.py
+++ For_Statements/For_program7.py
@@ -17,3 +17,12 @@ table_14 = []
 for i, j in enumerate(table_12):
     table_14.append(j+(i+1)*2)
 print(table_14)
+
+
+   OUTPUT:
+        
+        [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+        [14, 28, 42, 56, 70, 84, 98, 112, 126, 140]
+
+
+
