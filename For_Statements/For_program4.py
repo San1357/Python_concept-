e0ff1e8b9204bda813d2a9264b9359f64b44f8e2
San Ayush <41052723+San1357@users.noreply.github.com>
2021-02-16 10:51:58 +0530

    Create For_program4.py

---
+++ For_Statements/For_program4.py
@@ -0,0 +1,26 @@
+#Problem: Print all elements of a list using for loop.
+
+
+#CODE1:
+
+a = [1,2,3,4,5]
+for i in a:
+    print(i)
+    
+    
+CODE2:
+
+a = [1,2,3,4,5]
+for i in range(1,6):
+    print(i)
+    
+    
+'''    #OUTPUT:
+    
+    1
+    2
+    3
+    4
+    5
+    
+    '''
